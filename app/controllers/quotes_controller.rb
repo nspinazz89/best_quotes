12428d8f355fa5548589a93d4b0cf835afd18500
nspinazz89 <nspinazz89@gmail.com>
2020-02-06 21:44:02 -0600

    cleaning up best quotes

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,9 @@
+class QuotesController < Rulers::Controller
+  def a_quote
+    "The game is afoot! Follow your spirit and on this charge cry, 'God for Henry, England and Saint George'!"
+  end
+
+  def exception
+    raise "It's a disaster!"
+  end
+end
--- config.ru
+++ config.ru
@@ -1,3 +1,2 @@
 require './config/application'
-
 run BestQuotes::Application.new

42ac1e56d79ff9df532dd225d32d9aeaee59e627
nspinazz89 <nspinazz89@gmail.com>
2020-02-17 20:27:04 -0600

    use autoloader, add rerun gem

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,6 @@
 class QuotesController < Rulers::Controller
   def a_quote
-    "The game is afoot! Follow your spirit and on this charge cry, 'God for Henry, England and Saint George'!"
+    'The game is afoot! Follow your spirit and on this charge cry, "God for Henry, England and Saint George!"'
   end
 
   def exception
